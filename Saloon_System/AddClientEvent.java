bd6fb92a030fe8a30a2b520d43e1af8a2d02676b
Hlalela29 <hlalelamasuluke@gmail.com>
2022-10-27 07:34:10 -0700

    Add client Event

---
+++ Saloon_System/AddClientEvent.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+private class >
+    implements ActionListener
+    {
+	     final GUI this$0;
+
+	     public void actionPerformed(ActionEvent actionevent)
+	     {
+		      GUI.access$1200(GUI.this, "Client");
+	     }
+
+	     private vent()
+	     {
+		     this$0 = GUI.this;
+		     super();
+	     }
+    }
