8550450aae1f745388ddf9da40861e042717dcfa
Hlalela29 <hlalelamasuluke@gmail.com>
2022-10-26 17:35:02 -0700

    View Salary Event

---
+++ Saloon_System/ViewSalaryEvent.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+private class 
+    implements ActionListener
+    {
+	    final GUI this$0;
+
+	    public void actionPerformed(ActionEvent actionevent)
+	    {
+		    String s = "Name\tSurname\tSalary\nFirst\tEmployee\tR1200";
+		    GUI.access$900(GUI.this, "View Salary", s);
+	    }
+
+	    private ent()
+	    {
+		this$0 = GUI.this;
+                super();
+	    }
+    }
