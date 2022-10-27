6d7c0526490555d00c122f95b42d895440bdd795
Hlalela29 <hlalelamasuluke@gmail.com>
2022-10-27 07:54:32 -0700

    Add Employee Event

---
+++ Saloon_System/AddEmployeeEvent.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+private class nit>
+    implements ActionListener
+    {
+	    final GUI this$0;
+
+	     public void actionPerformed(ActionEvent actionevent)
+	     {
+		     GUI.access$1200(GUI.this, "Employee");
+	     }
+
+	      private onEvent()
+	      {
+		      this$0 = GUI.this;
+		      super();
+	      }
+    }
